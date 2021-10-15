2197d2e3147567f5971ceb6169ceed8d10b14812
Weston1 <westoneverett0@gmail.com>
2021-10-15 13:54:50 +0100

    Added Local Video display example

---
+++ js/local_vid.js
@@ -0,0 +1,19 @@
+'use strict';
+
+async function _play_video() {
+
+  const constraints = window.constraints = {
+    audio: true,
+    video: true
+  };
+
+  const video = document.querySelector('video');
+
+  try {
+    video.srcObject  = await navigator.mediaDevices.getUserMedia(constraints);
+  } catch (e) {
+    console.error("video error", e);
+  }
+}
+
+var play_video = LINKS.kify(_play_video);
