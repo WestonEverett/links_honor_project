0eb88a2eb08e97426d410e8b077f8d8a8faa51f0
Weston1 <westoneverett0@gmail.com>
2021-10-27 18:15:39 +0100

    Work on broacast system

--- js/vid_calling.js
+++ js/vid_calling.js
@@ -1,3 +1,32 @@
+let localName = '';
+let localID = '';
+
+function _setName() {
+  localName = prompt('name?', '')
+}
+
+function _getLocalName(){
+  return localName;
+}
+
+function _toStr(object) {
+  return object._value;
+}
+
+function _setID() {
+  localID = Math.floor((1. + Math.random()) * 10000000).toString().substring(1,9);
+}
+
+function _getID() {
+  return localID;
+}
+
+var getLocalName = LINKS.kify(_getLocalName);
+var setName = LINKS.kify(_setName);
+var toString = LINKS.kify(_toStr);
+var getID = LINKS.kify(_setID);
+var getID = LINKS.kify(_getID);
+
 async function _playLocalVideo(ID) {
 
   const constraints = window.constraints = {
@@ -12,6 +41,10 @@ async function _playLocalVideo(ID) {
   } catch (e) {
     console.error("video error", e);
   }
+}
+
+function _createOffer() {
+  
 }
 
 var playLocalVideo = LINKS.kify(_playLocalVideo);
