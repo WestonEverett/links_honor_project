2d4c77f64a17b01d455fa7f47092e07ea9e7ac81
Weston1 <westoneverett0@gmail.com>
2023-01-25 12:40:28 +0000

    Proper Seperation of CallClients in JS

--- MInf_Project2_planning/CallClientSeperation/js/sep_vidcalls.js
+++ MInf_Project2_planning/CallClientSeperation/js/sep_vidcalls.js
@@ -120,178 +120,121 @@ const offerOptions = {
   offerToReceiveVideo: 1
 };
 
-async function _createOffer(foreignID) {
+async function _createOffer(localID, foreignID) {
 
-  await preparePC(foreignID);
+  await preparePC(localID, foreignID);
 
-  var offer = await peerData[foreignID].pc.createOffer(offerOptions)
-  await peerData[foreignID].pc.setLocalDescription(offer);
+  var offer = await peerData[localID][foreignID].pc.createOffer(offerOptions)
+  await peerData[localID][foreignID].pc.setLocalDescription(offer);
 
-  peerData[foreignID].dataStr = JSON.stringify(offer);
+  peerData[localID][foreignID].dataStr = JSON.stringify(offer);
 
 }
 
-async function preparePC(foreignID){
-  peerData[foreignID] = {pc:"none", foreignID:foreignID, iceCandidates:[], dataStr:"wait", remoteStream: new MediaStream(), localStream: new MediaStream()}
+async function preparePC(localID, foreignID){
+  if (!(localID in peerData)) {
+    peerData[localID] = {};
+  }
+  peerData[localID][foreignID] = {pc:"none", foreignID:foreignID, iceCandidates:[], dataStr:"wait", remoteStream: new MediaStream(), localStream: new MediaStream()}
 
   var newVid = document.createElement('video');
   newVid.setAttribute('autoplay', 'true');
   newVid.setAttribute('object-fit', 'cover');
   newVid.setAttribute('width', '320px');
   newVid.setAttribute('height', '240px');
-  newVid.setAttribute('id', 'VidOf' + foreignID);
+  newVid.setAttribute('id', 'VidOf' + foreignID + 'For' + localID);
 
-  newVid.srcObject = peerData[foreignID].remoteStream;
+  newVid.srcObject = peerData[localID][foreignID].remoteStream;
 
   document.getElementById('vids').appendChild(newVid);
 
-  peerData[foreignID].pc = new RTCPeerConnection(configuration);
+  peerData[localID][foreignID].pc = new RTCPeerConnection(configuration);
 
-  peerData[foreignID].pc.ontrack = async (event) => {
-    peerData[foreignID].remoteStream.addTrack(event.track, peerData[foreignID].remoteStream);
+  peerData[localID][foreignID].pc.ontrack = async (event) => {
+    peerData[localID][foreignID].remoteStream.addTrack(event.track, peerData[localID][foreignID].remoteStream);
     console.log("remote track added");
   }
 
-  peerData[foreignID].pc.addEventListener('icecandidate', event => {
-    if (event.candidate) {peerData[foreignID].iceCandidates.push(event.candidate);}
+  peerData[localID][foreignID].pc.addEventListener('icecandidate', event => {
+    if (event.candidate) {peerData[localID][foreignID].iceCandidates.push(event.candidate);}
   });
 
-  peerData[foreignID].pc.addEventListener('connectionstatechange', event => {
-    console.log(peerData[foreignID].pc.connectionState);
-    if(peerData[foreignID].pc.connectionState == "disconnected"){
-      _hangup(foreignID);
+  peerData[localID][foreignID].pc.addEventListener('connectionstatechange', event => {
+    console.log(peerData[localID][foreignID].pc.connectionState);
+    if(peerData[localID][foreignID].pc.connectionState == "disconnected"){
+      _hangup(localID, foreignID);
     }
   });
 
   let localStream = await navigator.mediaDevices.getUserMedia(audVidConstraintsLocal);
 
-  peerData[foreignID].localStream = await navigator.mediaDevices.getUserMedia(audVidConstraints);
-  peerData[foreignID].localStream.getTracks().forEach((track) => {peerData[foreignID].pc.addTrack(track, localStream); console.log("track attached");});
+  peerData[localID][foreignID].localStream = await navigator.mediaDevices.getUserMedia(audVidConstraints);
+  peerData[localID][foreignID].localStream.getTracks().forEach((track) => {peerData[localID][foreignID].pc.addTrack(track, localStream); console.log("track attached");});
 }
 
-function _checkAsyncDone(foreignID) {
-  return peerData[foreignID].dataStr;
+function _checkAsyncDone(localID, foreignID) {
+  return peerData[localID][foreignID].dataStr;
 }
 
-function _hangupAll(){
-  for (peer in peerData) {
-    _hangup(peer);
+function _hangupAll(localID){
+  for (peer in peerData[localID]) {
+    _hangup(localID, peer);
   }
 }
 
-function _hangup(foreignID){
+function _hangup(localID, foreignID){
 
-  var remoteVideo = document.getElementById('VidOf' + foreignID);
+  var remoteVideo = document.getElementById('VidOf' + foreignID + 'For' + localID);
 
   if(remoteVideo != null){
     remoteVideo.parentNode.removeChild(remoteVideo);
-    peerData[foreignID].pc.close();
+    peerData[localID][foreignID].pc.close();
   }
   else {
-    console.log("can't find vid of " + foreignID);
+    console.log("can't find vid of " + foreignID + " in CallClient " + localID);
   }
 }
 
-async function _createAnswer(foreignID, newOfferStr) {
+async function _createAnswer(localID, foreignID, newOfferStr) {
 
 
   var offer = JSON.parse(newOfferStr);
 
-  await preparePC(foreignID);
+  await preparePC(localID, foreignID);
 
-  await peerData[foreignID].pc.setRemoteDescription(new RTCSessionDescription(offer));
-  var answer = await peerData[foreignID].pc.createAnswer();
-  await peerData[foreignID].pc.setLocalDescription(answer);
+  await peerData[localID][foreignID].pc.setRemoteDescription(new RTCSessionDescription(offer));
+  var answer = await peerData[localID][foreignID].pc.createAnswer();
+  await peerData[localID][foreignID].pc.setLocalDescription(answer);
 
-  peerData[foreignID].dataStr = JSON.stringify(answer);
+  peerData[localID][foreignID].dataStr = JSON.stringify(answer);
 }
 
-async function _createAccept(foreignID, newOfferStr) {
+async function _createAccept(localID, foreignID, newOfferStr) {
 
-  peerData[foreignID].dataStr = "wait";
+  peerData[localID][foreignID].dataStr = "wait";
 
   var offer = JSON.parse(newOfferStr);
 
-  await peerData[foreignID].pc.setRemoteDescription(new RTCSessionDescription(offer));
+  await peerData[localID][foreignID].pc.setRemoteDescription(new RTCSessionDescription(offer));
 
-  peerData[foreignID].dataStr = '';
+  peerData[localID][foreignID].dataStr = '';
 }
 
-function _checkForIceCandidates(foreignID){
-  if(peerData[foreignID].iceCandidates.length == 0){
+function _checkForIceCandidates(localID, foreignID){
+  if(peerData[localID][foreignID].iceCandidates.length == 0){
     return "None";
   }
   else{
-    return JSON.stringify(peerData[foreignID].iceCandidates.shift());
+    return JSON.stringify(peerData[localID][foreignID].iceCandidates.shift());
   }
 }
 
-async function _newRemoteCandidate(foreignID, iceCandidate){
+async function _newRemoteCandidate(localID, foreignID, iceCandidate){
   try {
     var cand = JSON.parse(iceCandidate)
-    await peerData[foreignID].pc.addIceCandidate(cand);
+    await peerData[localID][foreignID].pc.addIceCandidate(cand);
   } catch (e) {
     console.error('Error adding received ice candidate', e);
-  }
-}
-
-function _setMute(foreignID, toBool){
-  if(foreignID in peerData){
-    peerData[foreignID].remoteStream.getAudioTracks().forEach(track => track.enabled = !toBool);
-    console.log(peerData[foreignID].remoteStream.getAudioTracks().length + " tracks toggled");
-  }
-  else if(foreignID == localID) {
-    for(id in peerData){
-      peerData[id].localStream.getAudioTracks().forEach(track => track.enabled = !toBool);
-    }
-  }
-  else {
-    console.log("foreignID not found");
-  }
-}
-
-function _setHide(foreignID, toBool){
-  if(foreignID in peerData){
-    peerData[foreignID].remoteStream.getVideoTracks().forEach(track => track.enabled = !toBool);
-    console.log(peerData[foreignID].remoteStream.getVideoTracks().length + " tracks toggled");
-  }
-  else if(foreignID == localID) {
-    for(id in peerData){
-      peerData[id].localStream.getVideoTracks().forEach(track => track.enabled = !toBool);
-    }
-  }
-  else {
-    console.log("foreignID not found");
-  }
-}
-
-function _setDeaf(foreignID, toBool){
-  if(foreignID in peerData){
-    peerData[foreignID].localStream.getAudioTracks().forEach(track => track.enabled = !toBool);
-    console.log(peerData[foreignID].localStream.getAudioTracks().length + " tracks toggled");
-  }
-  else if(foreignID == localID) {
-    for(id in peerData){
-      peerData[id].remoteStream.getAudioTracks().forEach(track => track.enabled = !toBool);
-    }
-  }
-  else {
-    console.log("foreignID not found");
-  }
-}
-
-function _setBlind(foreignID, toBool){
-  if(foreignID in peerData){
-    peerData[foreignID].localStream.getVideoTracks().forEach(track => track.enabled = !toBool);
-    console.log(peerData[foreignID].remoteStream.getVideoTracks().length + " tracks toggled");
-  }
-  else if(foreignID == localID) {
-    for(id in peerData){
-      peerData[id].remoteStream.getVideoTracks().forEach(track => track.enabled = !toBool);
-    }
-  }
-  else {
-    console.log("foreignID not found");
   }
 }
 
@@ -301,11 +244,6 @@ var createAnswer = LINKS.kify(_createAnswer);
 var createAccept = LINKS.kify(_createAccept);
 var hangup = LINKS.kify(_hangup);
 var hangupAll = LINKS.kify(_hangupAll);
-
-var setMute = LINKS.kify(_setMute);
-var setHide = LINKS.kify(_setHide);
-var setDeaf = LINKS.kify(_setDeaf);
-var setBlind = LINKS.kify(_setBlind);
 
 var showInputs = LINKS.kify(_showInputs);
 var setInputs = LINKS.kify(_setInputs);
